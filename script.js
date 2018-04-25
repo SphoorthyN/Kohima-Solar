caa0446ae5cd8aa4f9e86492b6ecdd6d430fd19f
Sphoorthy <nlasphoorthy.code@gmail.com>
2018-04-25 23:43:02 +0530

    Set up the skeleton for the responsive styles and the VueJS menu templates

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
+// START OF VUE COMPONENTS
+
+// // navbar
+Vue.component("menu", {
+    template: ""
+});
+
+// END OF VUE COMPONENTS
+
 var vue = new Vue({
     el: "#vueApp",
     data: {
+        menus: [
+            {
+                id: 0,
+                menu: "home"
+            },
+            {
+                id: 1,
+                menu: "about"
+            },
+            {
+                id: 2,
+                menu: "projects"
+            },
+            {
+                id: 3,
+                menu: "contact"
+            }
+
+        ]
 
     },
-    methods: {
-
-    },
-    computed: {
-
-    }
-})
+    methods: {},
+    computed: {}
+});
